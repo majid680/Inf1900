801db4e33e0536baf3c163cd19d275d6e6861745
majid680 <majid.khauly@polymtl.ca>
2025-09-23 18:33:09 -0400

    Add files via upload

--- pb2.cpp
+++ pb2.cpp
@@ -5,7 +5,7 @@
 
 volatile uint8_t gMinuterieExpiree;
 volatile uint8_t gBoutonPoussoir; 
-
+constexpr int DUREE = 7812;
 
 void allumerRouge()
 {
@@ -17,11 +17,17 @@ void allumerVert()
     PORTA |= 1 << PA1;
     PORTA &= ~(1<< PA0);
 }
+
+void eteint()
+{
+    PORTA &= ~(1<< PA0);
+    PORTA &= ~(1<< PA1);
+}
 void clignoterRouge()
 {
-    for (i=0; i<10; i++)
+    for (int i=0; i<10; i++)
     {
-        rouge();
+        allumerRouge();
         _delay_ms(100);
         eteint();
         _delay_ms(900);
@@ -29,11 +35,7 @@ void clignoterRouge()
 }
 
 
-void eteint()
-{
-    PORTA &= ~(1<< PA0);
-    PORTA &= ~(1<< PA1);
-}
+
 
 ISR (TIMER1_COMPA_vect) 
 {
@@ -50,6 +52,17 @@ ISR (INT0_vect)
     EIFR |= (1 << INTF0) ;
 }
 
+
+void partirMinuterie ( uint16_t duree ) {
+    gMinuterieExpiree = 0;
+    TCNT1 = 0;
+    OCR1A = DUREE;
+    TCCR1A = 0 ;
+    TCCR1B |= (1 << CS02) | (1 << CS00);
+    TCCR1C = 0;
+    TIMSK1 |= (1 << ICIE1 );
+}
+
 int main()
 {
     cli();
@@ -57,9 +70,12 @@ int main()
     DDRD &= ~(1 << PD2);
     EIMSK |= (1 << INT0) ;
     EICRA |= (1 << ISC01) | (1 << ISC00) ; 
-    sei();
+    
 
     clignoterRouge();
+    sei();
+    
+    partirMinuterie(DUREE);
     do {
         allumerVert();
     } while ( gMinuterieExpiree == 0 && gBoutonPoussoir == 1);
@@ -68,9 +84,7 @@ int main()
     } while ( gMinuterieExpiree == 1);
 
     cli ();
-
-    // Verifier la réponse
-    'modifier ici'
+    
 
     return 0;
 }
